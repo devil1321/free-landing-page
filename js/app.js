c0f8bed151e05b97d62a6598095eb92775ec6c96
devil1321 <s.dominik1321@gmail.com>
2021-06-18 12:40:33 +0200

    init hero card one animation

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+const tl = gsap.timeline()
+// svg one card hero
+tl.to('.hero__card-1',{duration:0.5,opacity:1})
+tl.to('#Mask-one',{duration:0.5,opacity:1})
+tl.to('#Rectangle_503',{duration:0.5,width:124.349})
+tl.to('#Rectangle_507',{duration:0.5,width:82.017})
+tl.to('#Rectangle_504',{duration:0.5,width:387.597})
+tl.to('#Rectangle_505',{duration:0.5,width:300.289})
+tl.to('#Rectangle_506',{duration:0.5,width:230.177})
+
+
